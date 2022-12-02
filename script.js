f1cdce460a4e9e7a71ba8f96af65cce8f90f62cd
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-02 10:54:35 +0530

    Completed Showing Message of input Number

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = randomNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Enter a Number !';
+  } else if (guess === randomNumber) {
+    document.querySelector('.message').textContent = 'Correct ✨🎉';
+  } else if (guess > randomNumber) {
+    document.querySelector('.message').textContent = '🥵 Too High !';
+  } else if (guess < randomNumber) {
+    document.querySelector('.message').textContent = '🥶 Too Low !';
+  }
+});
